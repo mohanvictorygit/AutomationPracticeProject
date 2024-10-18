7ef13f2367bee6dbe86f5717e4ddc3c450c19a59
Mohankumar <hrmohanvictory@gmail.com>
2024-10-18 23:20:33 +0530

    Initial Commit

---
+++ src/test/java/testcase/SampleTest.java
@@ -0,0 +1,17 @@
+package testcase;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SampleTest 
+    
+{
+	@Test
+	public void TestFirst()
+	{
+		System.out.println("Test executed without errors");
+	}
+    
+}
